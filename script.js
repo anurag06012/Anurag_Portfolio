41b5e0aa414ebd0cd9793aa162dd9985454d496a
anurag06012 <125425765+anurag06012@users.noreply.github.com>
2024-03-26 17:26:50 +0530

    Anurag_Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Teacher","Full Stack Developer", "Data Analyst","Photographer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
